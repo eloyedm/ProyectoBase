590d32ae482ffde31b479a9b01a841e699a2c42b
eloyedm <eloy.edm@gmail.com>
2016-05-18 13:47:22 -0500

    reporte con query aplicado

--- Prueba proyecto bdat/frmUsoDeTarjetaPC.vb
+++ Prueba proyecto bdat/frmUsoDeTarjetaPC.vb
@@ -1,6 +1,6 @@
 ﻿Public Class frmUsoDeTarjetaPC
     Private Sub cargaReporte()
-        Me.dgridReporte.DataSource = ConnectionModule.connection.ReaderCommand("")
+        Me.dgridReporte.DataSource = ConnectionModule.connection.ReaderCommand("SELECT Comercio.nombreComercial,COUNT(Cargo.idCargo) AS NumeroCargos, SUM(Cargo.monto) AS totalCargos FROM Cargo LEFT JOIN Comercio ON Cargo.idComercio=Comercio.idComercio WHERE CONVERT(VARCHAR(30),fechaCargo, 126) BETWEEN '2016-04-10%' AND '2016-04-20%' GROUP BY nombreComercial", "Cargo").Tables("Cargo")
     End Sub
 
     Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs) Handles dgridReporte.CellContentClick
